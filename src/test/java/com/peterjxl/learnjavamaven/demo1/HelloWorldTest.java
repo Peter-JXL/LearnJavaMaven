54d8f16e7eee6a9ff424eef36c30c22093893a92
peterjxl <peterjxl@qq.com>
2023-04-12 21:04:14 +0800

    修复错误的包名

--- src/test/java/com/peterjxl/learnjavamaven/demo1/HelloWorldTest.java
+++ src/test/java/com/peterjxl/learnjavamaven/demo1/HelloWorldTest.java
@@ -1,11 +1,10 @@
-package com.peterjxl.learnjavamavn.demo1;
+package com.peterjxl.learnjavamaven.demo1;
 import org.junit.Test;
 
 public class HelloWorldTest {
 
     @Test
     public void testMain() {
-       HelloWorld helloWorld = new HelloWorld();
        HelloWorld.main(new String[10]);
     }
 }
